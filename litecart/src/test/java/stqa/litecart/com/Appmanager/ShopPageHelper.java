2658eb5f483ebbc9f2bf184051db0bcd511f9dd7
Go1em <gojiem777@gmail.com>
2018-03-26 22:14:13 +0300

    ht_11_updates

--- litecart/src/test/java/stqa/litecart/com/Appmanager/ShopPageHelper.java
+++ litecart/src/test/java/stqa/litecart/com/Appmanager/ShopPageHelper.java
@@ -25,28 +25,28 @@ public class ShopPageHelper extends HelperBase {
     }
 
     public void fillRegistrationForm(User user){
-        type(user.getTaxId(), By.cssSelector("div#create-account tr:nth-child(1) td:first-child input"));
-        type(user.getCompanyName(), By.cssSelector("div#create-account tr:nth-child(1) td:last-child input"));
+        type(user.getTaxId(), By.cssSelector("input[name=tax_id]"));
+        type(user.getCompanyName(), By.cssSelector("input[name=company]"));
 
-        type(user.getFirstName(), By.cssSelector("div#create-account tr:nth-child(2) td:first-child input"));
-        type(user.getLastName(), By.cssSelector("div#create-account tr:nth-child(2) td:last-child input"));
+        type(user.getFirstName(), By.cssSelector("input[name=firstname]"));
+        type(user.getLastName(), By.cssSelector("input[name=lastname]"));
 
-        type(user.getAddres1(), By.cssSelector("div#create-account tr:nth-child(3) td:first-child input"));
-        type(user.getAddres2(), By.cssSelector("div#create-account tr:nth-child(3) td:last-child input"));
+        type(user.getAddres1(), By.cssSelector("input[name=address1]"));
+        type(user.getAddres2(), By.cssSelector("input[name=address2]"));
 
-        type(user.getPostCode(), By.cssSelector("div#create-account tr:nth-child(4) td:first-child input"));
-        type(user.getCity(), By.cssSelector("div#create-account tr:nth-child(4) td:last-child input"));
+        type(user.getPostCode(), By.cssSelector("input[name=postcode]"));
+        type(user.getCity(), By.cssSelector("input[name=city]"));
 
         selectFromPicklist(By.cssSelector("select[name=country_code]"), user.getCountry());
         selectFromPicklist(By.cssSelector("select[name=zone_code]"), user.getState());
 
-        type(user.getEmail(), By.cssSelector("div#create-account tr:nth-child(6) td:first-child input"));
-        type(user.getPhone(), By.cssSelector("div#create-account tr:nth-child(6) td:last-child input"));
+        type(user.getEmail(), By.cssSelector("input[name=email]"));
+        type(user.getPhone(), By.cssSelector("input[name=phone]"));
 
         isCheck(By.cssSelector("input[name=newsletter]"));
 
-        type(user.getDesiredPassword(), By.cssSelector("div#create-account tr:nth-child(8) td:first-child input"));
-        type(user.getDesiredPassword(), By.cssSelector("div#create-account tr:nth-child(8) td:last-child input"));
+        type(user.getDesiredPassword(), By.cssSelector("input[name=password]"));
+        type(user.getDesiredPassword(), By.cssSelector("input[name=confirmed_password]"));
 
     }
 
@@ -56,9 +56,6 @@ public class ShopPageHelper extends HelperBase {
         WebElement button = getWebElement(By.cssSelector("button[name=login]"));
         executeJS("arguments[0].scrollIntoView(true);",button);
         executeJS("arguments[0].click();",button);
-//        executor.executeScript("arguments[0].scrollIntoView(true);",button);
-//        executor.executeScript("arguments[0].click();",button);
-
     }
 
 
